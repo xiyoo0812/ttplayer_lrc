ba91765c43a19d42f0834809b922f88ba5bc28ca
xiyoo0812 <xiyoo0812@163.com>
2022-05-13 10:27:02 +0800

    添加忘記提交的文件

---
+++ script/sandbox.lua
@@ -0,0 +1,107 @@
+--sandbox.lua
+require("lualog")
+local lstdfs    = require("lstdfs")
+
+local pairs     = pairs
+local loadfile  = loadfile
+local iopen     = io.open
+local mabs      = math.abs
+local tpack     = table.pack
+local tunpack   = table.unpack
+local sformat   = string.format
+local dgetinfo  = debug.getinfo
+local file_time = lstdfs.last_write_time
+
+local logger    = quanta.get_logger()
+
+local load_files    = {}
+local search_path   = {}
+
+local function ssplit(str, token)
+    local t = {}
+    while #str > 0 do
+        local pos = str:find(token)
+        if pos then
+            t[#t+1] = str:sub(1, pos - 1)
+            str = str:sub(pos + 1, #str)
+        else
+            t[#t + 1] = str
+            break
+        end
+    end
+    return t
+end
+
+--加载lua文件搜索路径
+for _, path in ipairs(ssplit(package.path, ";")) do
+    search_path[#search_path + 1] = path:sub(1, path:find("?") - 1)
+end
+
+local function search_load(node)
+    local load_path = node.fullpath
+    if load_path then
+        node.time = file_time(load_path)
+        return loadfile(load_path)
+    end
+    local filename = node.filename
+    for _, path_root in pairs(search_path) do
+        local fullpath = path_root .. filename
+        local file = iopen(fullpath)
+        if file then
+            file:close()
+            node.fullpath = fullpath
+            node.time = file_time(fullpath)
+            return loadfile(fullpath)
+        end
+    end
+    return nil, "file not exist!"
+end
+
+local function try_load(node)
+    local trunk_func, err = search_load(node)
+    if not trunk_func then
+        logger.error(sformat("[sandbox][try_load] load file: %s ... [failed]\nerror : %s", node.filename, err))
+        return
+    end
+    local res = tpack(pcall(trunk_func))
+    if not res[1] then
+        logger.error(sformat("[sandbox][try_load] exec file: %s ... [failed]\nerror : %s", node.filename, res[2]))
+        return
+    end
+    logger.info(sformat("[sandbox][try_load] load file: %s ... [ok]", node.filename))
+    return tunpack(res, 2)
+end
+
+function import(filename)
+    local node = load_files[filename]
+    if not node then
+        node = { filename = filename }
+        load_files[filename] = node
+    end
+    if not node.time then
+        local res = try_load(node)
+        if res then
+            node.res = res
+        end
+    end
+    return node.res
+end
+
+function quanta.reload()
+    for path, node in pairs(load_files) do
+        local filetime = file_time(node.fullpath)
+        if mabs(node.time - filetime) > 1 then
+            try_load(node)
+        end
+    end
+end
+
+function quanta.get(name)
+    local global_obj = quanta[name]
+    if not global_obj then
+        local info = dgetinfo(2, "S")
+        logger.error(sformat("[quanta][get] %s not initial! source(%s:%s)", name, info.short_src, info.linedefined))
+        return
+    end
+    return global_obj
+end
---
+++ script/store/redis_mgr.lua
@@ -0,0 +1,61 @@
+--redis_mgr.lua
+local tpack         = table.pack
+local log_err       = logger.err
+local qget          = quanta.get
+local qenum         = quanta.enum
+
+local event_mgr     = qget("event_mgr")
+local config_mgr    = qget("config_mgr")
+
+local SUCCESS       = qenum("KernCode", "SUCCESS")
+local REDIS_FAILED  = qenum("KernCode", "REDIS_FAILED")
+
+local RedisMgr = singleton()
+local prop = property(RedisMgr)
+prop:accessor("redis_dbs", {})      -- redis_dbs
+prop:accessor("default_db", nil)    -- default_db
+
+function RedisMgr:__init()
+    self:setup()
+    -- 注册事件
+    event_mgr:add_listener(self, "redis_execute", "execute")
+end
+
+--初始化
+function RedisMgr:setup()
+    local RedisDB = import("driver/redis.lua")
+    local database = config_mgr:init_table("database", "db", "driver")
+    for _, conf in database:iterator() do
+        if conf.driver == "redis" then
+            local redis_db = RedisDB(conf)
+            self.redis_dbs[conf.db] = redis_db
+            if conf.default then
+                self.default_db = redis_db
+            end
+        end
+    end
+end
+
+--查找redis db
+function RedisMgr:get_db(db_name)
+    if not db_name or db_name == "default" then
+        return self.default_db
+    end
+    return self.redis_dbs[db_name]
+end
+
+function RedisMgr:execute(db_name, cmd, ...)
+    local redisdb = self:get_db(db_name)
+    if redisdb then
+        local ok, res_oe = redisdb:execute(cmd, ...)
+        if not ok then
+            log_err("[RedisMgr][execute] execute %s (%s) failed, because: %s", cmd, tpack(...), res_oe)
+        end
+        return ok and SUCCESS or REDIS_FAILED, res_oe
+    end
+    return REDIS_FAILED, "redis db not exist"
+end
+
+quanta.redis_mgr = RedisMgr()
+
+return RedisMgr
